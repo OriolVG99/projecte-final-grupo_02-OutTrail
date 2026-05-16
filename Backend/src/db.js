87a14672b980b8de4b9a86713684a439d0b4386c
Oriol Vidal G <ovidalg@fp.insjoaquimmir.cat>
2026-05-16 18:04:22 +0200

    Errores

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -1,11 +1,13 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import pg from 'pg';
 
 dotenv.config();
 
 //Configuracio de la connexio a la base de dades PostgreSQL amb Sequelize
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
